fa7b076961bc2af4cd26914285f8c2c7893be37d
Talather <atherjawad11@gmail.com>
2025-01-09 20:57:21 +0500

    vv

--- src/pages/reward-tracker.tsx
+++ src/pages/reward-tracker.tsx
@@ -4,7 +4,8 @@ import ChartR from "../components/chartR";
 import TableCredits from "@/components/TableOneForCredits";
 import { useAuth } from "@/context/AuthContext";
 import { fetchCreditHistoryForSpecificUser } from "@/lib/clientControllers/userSpecificAssets";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
+import CenteredBoxWithButtons from '@/components/addOrSendCreditCard'
 const RewardTracker = () => {
 
 
@@ -76,18 +77,29 @@ const{user}=useAuth()
         </h2>
         <ChartR />
       </div>
-      <div className="mt-16 bg-white">
-        <div className="text-center">
-          <h3 className="text-2xl font-semibold text-gray-800 md:text-4xl">
-            Credits Earned:
-          </h3>
-        </div>
-        <div className="flex items-center justify-center mt-12 mb-10">
-          <div className="w-full max-w-4xl p-4 border border-gray-200 rounded-lg shadow-lg">
-            {creditDocs?.length && <TableCredits creditDocs={creditDocs} />}
+
+
+      {creditDocs?.length ? (
+        <div className="mt-16 bg-white">
+          <div className="text-center">
+            <h3 className="text-2xl font-semibold text-gray-800 md:text-4xl">
+              Credits Earned:
+            </h3>
           </div>
-        </div>
-      </div>
+          <div className="flex items-center justify-center mt-12 mb-10">
+            <div className="w-full max-w-4xl p-4 border border-gray-200 rounded-lg shadow-lg">
+              <TableCredits creditDocs={creditDocs} />
+            </div>
+          </div>
+        </div>) : <div></div>}
+      <div className='flex flex-col items-center justify-center px-4 py-16 bg-button-gpt-hover'>
+  <h2 className='mb-6 text-3xl font-semibold text-white sm:text-4xl'>
+    Credits Transaction
+  </h2>
+  <CenteredBoxWithButtons
+ />
+</div>
+
 
       {/* Hero Section */}
     </div>
